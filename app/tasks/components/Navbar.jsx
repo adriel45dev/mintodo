d9863a4f45c0b8e245f3401281be872057e87015
adriel45dev <45adrielalves45@gmail.com>
2023-09-05 14:18:40 -0300

    Change Logout Button Color

--- app/tasks/components/Navbar.jsx
+++ app/tasks/components/Navbar.jsx
@@ -21,7 +21,7 @@ export default function Navbar() {
           <div>
             <Link
               href={"/"}
-              className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2 "
+              className="focus:outline-none text-white bg-gray-700 hover:bg-gray-800 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2 "
             >
               Logout
             </Link>
